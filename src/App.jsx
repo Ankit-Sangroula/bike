a9fe60c46c450253a794aadff772884113d80c48
Ankit-Sangroula <ankitsangroula2006@gmail.com>
2026-02-12 14:54:03 +0545

    Replace old code with new code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+       <h1 className='bg-red-600 text-4xl'>Hello</h1>
+       <div className='animate-spin text-2xl'>A</div>
+    </div>
+  )
+}
+
+export default App
